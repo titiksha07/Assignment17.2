8e3f4e73d22a871e9ace6161078250dd4dd26a94
rikrivu <30924294+rikrivu@users.noreply.github.com>
2017-10-08 23:01:10 +0530

    First Commit

---
+++ src/app/employee-interface.ts
@@ -0,0 +1,16 @@
+export interface EmployeeInterface
+{
+    firstName:string;
+    lastName:string;
+    employeeId:number;
+    empMail:string;
+    description:string;
+}
+export interface EmployeeInterfaceModel
+{
+    firstName:string;
+    lastName:string;
+    employeeId:number;
+    empMail:string;
+   department:string;
+}
